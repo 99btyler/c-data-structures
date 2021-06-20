82d6dd63aa9d7feca4b86f4826ad10c549aa3aeb
Tyler Beringer <99tylerberinger@gmail.com>
2021-06-20 16:03:12 -0400

    Created DoublyLinkedList with Node

---
+++ src/other/DoublyLinkedList.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+struct Node {
+    int data;
+    struct Node *nextNode;
+    struct Node *previousNode;
+};
+
+int main() {
+
+    printf("\nDoublyLinkedList\n");
+
+    return 0;
+
+}
