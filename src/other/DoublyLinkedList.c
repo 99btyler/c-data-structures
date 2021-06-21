7ee6d741fdf49072f466b8db56dc1156e909e566
Tyler Beringer <99tylerberinger@gmail.com>
2021-06-21 19:15:46 -0400

    Added methods add() and printDoublyLinkedList()

--- src/other/DoublyLinkedList.c
+++ src/other/DoublyLinkedList.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 struct Node {
     int data;
@@ -6,9 +7,54 @@ struct Node {
     struct Node *previousNode;
 };
 
+void printDoublyLinkedList(struct Node *node) {
+
+    printf("null <=> ");
+
+    while (node != NULL) {
+        printf("[%d]%d <=> ", (node->previousNode != NULL ? node->previousNode->data : -1), node->data);
+        node = node->nextNode;
+    }
+
+    printf("null\n");
+
+}
+
+void add(int data, struct Node **firstNode) {
+
+    struct Node *newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->nextNode = NULL;
+    newNode->previousNode = NULL;
+
+    if (*firstNode == NULL) {
+
+        *firstNode = newNode;
+
+    } else {
+
+        struct Node *lastNode = *firstNode;
+        while (lastNode->nextNode != NULL) {
+            lastNode = lastNode->nextNode;
+        }
+
+        lastNode->nextNode = newNode;
+        newNode->previousNode = lastNode;
+
+    }
+
+    printDoublyLinkedList(*firstNode);
+
+}
+
 int main() {
 
     printf("\nDoublyLinkedList\n");
+
+    struct Node *firstNode = NULL;
+
+    add(17, &firstNode);
+    add(19, &firstNode);
 
     return 0;
 
