3a12d45de4ba89d49885319732a99f533481f058
Tyler Beringer <99tylerberinger@gmail.com>
2021-06-28 22:51:58 -0400

    Created CircularDoublyLinkedList with Node

---
+++ src/other/CircularDoublyLinkedList.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+struct Node {
+    int data;
+    struct Node *nextNode;
+    struct Node *previousNode;
+};
+
+int main() {
+
+    printf("\nCircularDoublyLinkedList\n");
+
+    struct Node *firstNode = NULL;
+
+    return 0;
+
+}
