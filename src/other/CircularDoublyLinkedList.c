0f1d1f8e513885de1fabd4fb3d17d6fbaf9f5ed2
Tyler Beringer <99tylerberinger@gmail.com>
2021-06-29 17:32:36 -0400

    Added printCircularDoublyLinkedList()

--- src/other/CircularDoublyLinkedList.c
+++ src/other/CircularDoublyLinkedList.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 struct Node {
     int data;
@@ -6,11 +7,29 @@ struct Node {
     struct Node *previousNode;
 };
 
+void printCircularDoublyLinkedList(struct Node *firstNode) {
+
+    struct Node *node = firstNode;
+    while (node != NULL) {
+        printf("[%d]%d[%d] ", (node->previousNode != NULL ? node->previousNode->data : -1), node->data, (node->nextNode != NULL ? node->nextNode->data : -1));
+        node = node->nextNode;
+        if (node == firstNode) {
+            break;
+        }
+    }
+
+    printf("...\n");
+
+}
+
 int main() {
 
     printf("\nCircularDoublyLinkedList\n");
 
     struct Node *firstNode = NULL;
+    
+    add(1, &firstNode);
+    add(3, &firstNode);
 
     return 0;
 
