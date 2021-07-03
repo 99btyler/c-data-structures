44ff95a6c969d18d2e46be719fde1b24ede2657b
Tyler Beringer <99tylerberinger@gmail.com>
2021-07-02 23:53:25 -0400

    Added Node

--- src/BinarySearchTree.c
+++ src/BinarySearchTree.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 
+struct Node {
+    int data;
+    struct Node *leftChildNode;
+    struct Node *rightChildNode;
+};
+
 int main() {
 
     printf("\nBinarySearchTree\n");
+
+    struct Node *firstNode = NULL;
 
     return 0;
 
