657b5e704de3e9b59e865e8fc800a9a5ac81c8fc
Tyler Beringer <99tylerberinger@gmail.com>
2021-07-06 14:23:33 -0400

    Added get() and indexOf()

--- src/other/CircularDoublyLinkedList.c
+++ src/other/CircularDoublyLinkedList.c
@@ -22,6 +22,48 @@ void printCircularDoublyLinkedList(struct Node *firstNode) {
 
 }
 
+int get(int index, struct Node **firstNode) {
+
+    struct Node *indexNode = *firstNode;
+    int i = 0;
+    while (indexNode != NULL) {
+        if (i == index) {
+
+            return indexNode->data;
+
+        }
+        indexNode = indexNode->nextNode;
+        i += 1;
+        if (indexNode == *firstNode) {
+            break;
+        }
+    }
+
+    return -1;
+
+}
+
+int indexOf(int data, struct Node **firstNode) {
+
+    struct Node *dataNode = *firstNode;
+    int i = 0;
+    while (dataNode != NULL) {
+        if (dataNode->data == data) {
+
+            return i;
+
+        }
+        dataNode = dataNode->nextNode;
+        i += 1;
+        if (dataNode == *firstNode) {
+            break;
+        }
+    }
+
+    return -1;
+
+}
+
 void add(int data, struct Node **firstNode) {
 
     struct Node *newNode = (struct Node*)malloc(sizeof(struct Node));
@@ -116,6 +158,8 @@ int main() {
     add(1, &firstNode);
     add(3, &firstNode);
     insert(2, 1, &firstNode);
+    printf("Data at index 0: %d\n", get(0, &firstNode));
+    printf("Index of data 3: %d\n", indexOf(3, &firstNode));
 
     return 0;
 
